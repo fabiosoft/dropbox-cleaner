f00143b9e913c6097c1d54b6ac6f44c2be425d63
andrewrgarcia <andrewrgarcia@ufl.edu>
2022-05-18 18:22:11 -0400

    patch; short-and-sweet fixes bug caused by new Dropbox versions

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-Fix Dropbox conflicts automatically.
+## Fix Dropbox conflicts automatically
+
+This script will climb through the **folder tree** and repair all conflict files in that folder IN BULK
+
+Andrew's patch takes script from [ciri](https://stackoverflow.com/users/3263897/ciri) and adds a global name conversion of all directories in folder tree to fix sed case sensitive rename bug caused by Case Conflict syntax in new Dropbox versions (2022)
+
+### Recommended
+
+Make `dropbox-cleaner.sh` file global by declaring its path on .bashrc
+
+Give it an alias in .bashrc i.e. `alias dbox-cleaner='/path-to-dropbox-cleaner/dropbox-cleaner.sh'`
+
+### Usage (with recommendations above)
+
+```ruby 
+cd foldertree-with-case-conflict-folders
+dbox-cleaner
+```
+
+
+
--- dropbox-cleaner.sh
+++ dropbox-cleaner.sh
@@ -1,63 +1,44 @@
 #!/bin/bash
-
-#Thx, http://stackoverflow.com/questions/20723868/batch-rename-dropbox-conflict-files
-
-#Point to where you want the script to look and where it should backup files it replaces
-folder="$(pwd)"
-backup=~/Desktop/Dropbox.backup
-
-#colors
-red='\033[0;31m'
-purple='\033[1;35m'
-NC='\033[0m' # No Color
+'''References:
+http://stackoverflow.com/questions/20723868/batch-rename-dropbox-conflict-files
+https://community.spiceworks.com/how_to/111611-how-to-change-all-files-folders-to-lowercase-unix
+'''
 
 clear
 
+#Point to where you want the script to look and where it should backup files it replaces
+folder="$(pwd)"
 
-echo "This script will climb through the $folder tree and repair conflict files by deleting the OLDER of the the conflicted file and its counterpart"
-if [[ $1 == "replace" ]]; then
-    echo -e "${red}This is NOT a drill.${NC} The script will backup the older of the conflicted files and then delete it from the Dropbox directory."
-else
-    echo -e "${purple}This is a dry run.${NC} You'll see what files would be replaced. Run \"./Dropbox.sh replace\" to make it run for real."
-fi
-echo "Press any key to continue..."
-echo "------------------------------"
+#make all folder names (including case conflict folders yes) lowercase
+rename 'y/A-Z/a-z/' *
+
+
+echo "This script will climb through the $folder tree and repair conflict files"
+echo "Press a key to continue..."
 read -n 1
+echo "------------------------------"
 
 find "$folder" -type f -print0 | while read -d $'\0' file; do
-    newname=$(echo "$file" | sed 's/ (.*conflitto.*)//')
+    newname=$(echo "$file" | sed 's/ (.*case conflict.*)//')   
 
     if [ "$file" != "$newname" ]; then
-        if [ -f "$newname" ];then
-            # determine which is newer
-            if [ "$newname" -nt "$file" ]; then
-                echo "$newname is NEWER than $file"
-                file_to_move="$file"
-                file_to_keep="$newname"
-            else
-                echo "$newname is OLDER than $file"
-                file_to_move="$newname"
-                file_to_keep="$file"
-            fi
-
-            backupname=${newname/"$folder"/"$backup"}
-
-            if [[ $1 != "replace" ]]; then
-                echo "Would have moved $file_to_move to $backupname"
-            else
-                echo "Moving $file_to_move to $backupname"
-                mkdir -p "$(dirname "$backupname")"
-                cp "$file_to_move" "$backupname"
-                mv "$file_to_keep" "$newname"
-            fi
-        else
-            # if the unconflicted version isn't there for some reason, just rename the original
-            if [[ $1 != "replace" ]]; then
-                echo "Didn't see an unconflicted counterpart for $file, so would have just renamed file"
-            else
-                echo "Didn't see an unconflicted counterpart for $file, so will replace"
-                mv "$file" "$newname"
-            fi
+        echo "Found conflict file - $file"
+        
+        if test -f "$newname"
+        then
+            backupname=$newname.backup
+            echo " "
+            echo "File with original name already exists, backup as $backupname"
+            mv "$newname" "$backupname"
         fi
-    fi
+
+        echo "moving $file to $newname"
+
+        mv "$file" "$newname"
+
+        echo
+fi
 done
+
+#make all folder names uppercase (if desired or needed)
+rename 'y/a-z/A-Z/' *
